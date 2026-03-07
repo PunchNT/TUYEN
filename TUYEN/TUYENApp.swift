15f4a34e8d8072572ed1f8aa229dfb364b93bd4b
PN <punch2620@gmail.com>
2026-03-07 19:59:01 +0700

    Initial Commit

---
+++ TUYEN/TUYENApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUYENApp.swift
+//  TUYEN
+//
+//  Created by Nattakorn Totone on 7/3/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct TUYENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
